e4259a1c289ebc4c08358a28e0c5f197aa0ea3e8
Akshay Manjunath <157276831+Brocoder07@users.noreply.github.com>
2025-05-14 01:20:12 +0530

    Add files via upload
    Added Data transfer objects to user

---
+++ backend/src/main/java/com/example/ecommerce/ecommerce_backend/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.example.ecommerce.ecommerce_backend.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
